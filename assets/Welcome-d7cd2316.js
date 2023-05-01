b52f5d21cd88dee4ea1075d71eb9a36436d1928e
steffanhalv <steffanhalv@users.noreply.github.com>
2023-05-01 13:20:21 +0000

    deploy: 719eb500d8805e55c97c2610494eb627beba634b

--- assets/Welcome-d7cd2316.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as n,c as s,a as r,b as a}from"./index-71ed55b5.js";const c={},l={style:{margin:"auto","font-weight":"bold","font-size":"25px",color:"navy"}};function p(o,e,d,i,u,f){return n(),s("div",l,[r(" Welcome to VuePlayground ! "),a("button",{type:"",onClick:e[0]||(e[0]=_=>o.$router.push("/hello"))}," Go to hello ")])}const $=t(c,[["render",p]]);export{$ as default};

4d0d403931724d1a12201dca4062a0adc4f529e2
Vue Play <author@vueplay.com>
2025-07-18 14:39:55 +0200

    Stored by Vue Play

--- src/views/ReleaseLog.vue
+++ src/views/ReleaseLog.vue
@@ -34,7 +34,7 @@
 							backgroundColor="{`default`:{`xs`:{`light`:`color-mix(in oklch, var(--color-neutral) 0%, white)`,`dark`:`color-mix(in oklch, var(--color-neutral) 10%, black)`}}}"
 							column="{`default`:{`xs`:{`light`:true}}}"
 						>
-							<h3>07.04.2025 - Release 1.5.3</h3>
+							<h3>18.07.2025 - Release 1.5.3</h3>
 							<ul class="indent-0 ml-5 list-disc">
 								<li>Feature - Build log</li>
 								<li>Feature - Design update for main menu</li>

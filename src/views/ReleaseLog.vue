c263dcdd37daf1720ab69d79f4e1b3ee552eb46c
Vue Play <author@vueplay.com>
2025-08-16 17:12:17 +0200

    Stored by Vue Play

--- src/views/ReleaseLog.vue
+++ src/views/ReleaseLog.vue
@@ -34,6 +34,24 @@
 							backgroundColor="{`default`:{`xs`:{`light`:`color-mix(in oklch, var(--color-neutral) 0%, white)`,`dark`:`color-mix(in oklch, var(--color-neutral) 10%, black)`}}}"
 							column="{`default`:{`xs`:{`light`:true}}}"
 						>
+							<h3>16.08.2025 - Release 1.5.7</h3>
+							<ul class="indent-0 ml-5 list-disc">
+								<li>Feature - New toolbox view</li>
+								<li>Feature - Switch between visual & code view</li>
+								<li>Feature - Code & visual split view</li>
+								<li>Feature - Editor tabs</li>
+								<li>Feature - Git tool to track diffs & changes</li>
+								<li>Feature - Visual npm dependency manager</li>
+								<li>Feature - Shortcut for easier deployments</li>
+								<li>Feature - More user friendly launch tool</li>
+								<li>Feature - Improved navigation for mobile devices</li>
+							</ul>
+						</Box>
+						<Box
+							class="shadow mb-6 rounded-lg bg-white p-6"
+							backgroundColor="{`default`:{`xs`:{`light`:`color-mix(in oklch, var(--color-neutral) 0%, white)`,`dark`:`color-mix(in oklch, var(--color-neutral) 10%, black)`}}}"
+							column="{`default`:{`xs`:{`light`:true}}}"
+						>
 							<h3>05.08.2025 - Release 1.5.5 ~ 1.5.6</h3>
 							<ul class="indent-0 ml-5 list-disc">
 								<li>Feature - Add transform origin pivot</li>

e1593f5cd3fb2f9321d7a72688cbb458410a0168
Vue Play <author@vueplay.com>
2025-09-15 15:03:14 +0200

    Stored by Vue Play

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
 	<Home>
-		<template #header="">
+		<template #header>
 			<Header style="position: sticky; top: 0px;z-index:1"></Header>
-		</template> <template #default="">
+		</template>
+		<template #default>
 			<Box
 				style="justify-content:center;padding:0px;min-height:calc(100vh - 60px)"
 				expand="true"
@@ -20,7 +21,9 @@
 						type="{`default`:{`xs`:{`light`:`h1`},`2xl`:{`light`:`h1`}}}"
 						lineHeight="{`default`:{`xs`:{`light`:`47px`},`sm`:{`light`:`67px`},`md`:{`light`:`72px`},`lg`:{`light`:`96px`}}}"
 						fontWeight="{`default`:{`xs`:{`light`:`250`}}}"
-					>Build Visually,<br />Create Anything</Title>
+					>Build Visually,
+						<br />Create Anything
+					</Title>
 					<Title
 						style="text-align:center"
 						type="{`default`:{`xs`:{`light`:`h2`},`2xl`:{`light`:`h2`}}}"
@@ -28,7 +31,9 @@
 						lineHeight="{`default`:{`xs`:{`light`:`25px`},`md`:{`light`:`28px`},`lg`:{`light`:`30px`}}}"
 						marginTop="{`default`:{`xs`:{`light`:`25px`}}}"
 						fontWeight="{`default`:{`xs`:{`light`:`325`}}}"
-					>Design, develop & launch websites, apps, components, and more —<br />rediscover the joy of building, from syntax to creativity.</Title>
+					>Design, develop & launch websites, apps, components, and more —
+						<br />rediscover the joy of building, from syntax to creativity.
+					</Title>
 					<Link
 						title="Get Started"
 						backgroundColor="{`hover`:{`xs`:{`dark`:`color-mix(in oklch, var(--color-primary) 60%, black)`}}}"
@@ -94,7 +99,8 @@
 								marginTop="{`default`:{`xs`:{`light`:`12px`}}}"
 								marginBottom="{`default`:{`xs`:{`light`:`12px`}}}"
 								style="color:#f8d2fcff;text-shadow:1px 2px 1px #171616ff"
-							>Design</Title><Text
+							>Design</Title>
+							<Text
 								fontSize="{`default`:{`xs`:{`light`:`16px`}}}"
 								lineHeight="{`default`:{`xs`:{`light`:`25px`}}}"
 								fontWeight="{`default`:{`xs`:{`light`:`325`}}}"
@@ -130,7 +136,8 @@
 								marginTop="{`default`:{`xs`:{`light`:`12px`}}}"
 								marginBottom="{`default`:{`xs`:{`light`:`12px`}}}"
 								style="color:#e1faf4ff;text-shadow:1px 2px 1px #171616ff"
-							>Develop</Title><Text
+							>Develop</Title>
+							<Text
 								fontSize="{`default`:{`xs`:{`light`:`16px`}}}"
 								lineHeight="{`default`:{`xs`:{`light`:`25px`}}}"
 								fontWeight="{`default`:{`xs`:{`light`:`325`}}}"
@@ -171,7 +178,8 @@
 								marginTop="{`default`:{`xs`:{`light`:`12px`}}}"
 								marginBottom="{`default`:{`xs`:{`light`:`12px`}}}"
 								style="color:#fceae6ff;text-shadow:1px 2px 1px #171616ff"
-							>Launch</Title><Text
+							>Launch</Title>
+							<Text
 								fontSize="{`default`:{`xs`:{`light`:`16px`}}}"
 								lineHeight="{`default`:{`xs`:{`light`:`25px`}}}"
 								fontWeight="{`default`:{`xs`:{`light`:`325`}}}"
@@ -218,7 +226,8 @@
 							>
 								<div></div>
 							</Box>
-						</Box><img
+						</Box>
+						<img
 							src="@/assets/screenshot.webp"
 							style="border-radius:3px"
 						/>
@@ -249,7 +258,8 @@
 								marginTop="{`default`:{`xs`:{`light`:`12px`}}}"
 								marginBottom="{`default`:{`xs`:{`light`:`12px`}}}"
 								lineHeight="{`default`:{`xs`:{`light`:`100px`}}}"
-							>{{formatCount(usersCount)}}</Title><Text
+							>{{formatCount(usersCount)}}</Title>
+							<Text
 								fontSize="{`default`:{`xs`:{`light`:`16px`}}}"
 								lineHeight="{`default`:{`xs`:{`light`:`25px`}}}"
 								fontWeight="{`default`:{`xs`:{`light`:`325`}}}"
@@ -273,7 +283,8 @@
 								marginTop="{`default`:{`xs`:{`light`:`12px`}}}"
 								marginBottom="{`default`:{`xs`:{`light`:`12px`}}}"
 								lineHeight="{`default`:{`xs`:{`light`:`100px`}}}"
-							>{{formatCount(websitesCount)}}</Title><Text
+							>{{formatCount(websitesCount)}}</Title>
+							<Text
 								fontSize="{`default`:{`xs`:{`light`:`16px`}}}"
 								lineHeight="{`default`:{`xs`:{`light`:`25px`}}}"
 								fontWeight="{`default`:{`xs`:{`light`:`325`}}}"
@@ -297,7 +308,8 @@
 								marginTop="{`default`:{`xs`:{`light`:`12px`}}}"
 								marginBottom="{`default`:{`xs`:{`light`:`12px`}}}"
 								lineHeight="{`default`:{`xs`:{`light`:`100px`}}}"
-							>{{formatCount(componentsCount)}}</Title><Text
+							>{{formatCount(componentsCount)}}</Title>
+							<Text
 								fontSize="{`default`:{`xs`:{`light`:`16px`}}}"
 								lineHeight="{`default`:{`xs`:{`light`:`25px`}}}"
 								fontWeight="{`default`:{`xs`:{`light`:`325`}}}"
@@ -322,7 +334,8 @@
 							alignItems="{`default`:{`xs`:{`light`:`center`}}}"
 							overflow="{`default`:{`xs`:{`light`:`visible`}}}"
 							padding="{`default`:{`xs`:{`light`:`0px`}}}"
-						><a
+						>
+							<a
 								class="bg-slate-50 hover:bg-slate-100 rounded-lg"
 								style="width:250px;display:block;border:1px;border-style:solid;border-color:#333;color:#333;margin-left:auto;margin-right:auto"
 								href="https://github.com/marketplace/vue-play-studio"
@@ -337,9 +350,12 @@
 									class="mt-1"
 								>
 									<path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-								</svg> <span style="padding-top:13px;display:inline-block;font-weight:bold;padding-bottom:12px;margin-left:20px">
+								</svg>
+								<span style="padding-top:13px;display:inline-block;font-weight:bold;padding-bottom:12px;margin-left:20px">
 									GitHub marketplace
-								</span></a> </Box>
+								</span>
+							</a>
+						</Box>
 						<Box
 							borderRadius="{`default`:{`xs`:{`light`:`8px`}}}"
 							column="{`default`:{`xs`:{`light`:true}}}"
@@ -348,16 +364,20 @@
 							alignItems="{`default`:{`xs`:{`light`:`center`}}}"
 							overflow="{`default`:{`xs`:{`light`:`visible`}}}"
 							padding="{`default`:{`xs`:{`light`:`0px`}}}"
-						><a
+						>
+							<a
 								href="https://www.producthunt.com/products/vueplay?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-vue&#0045;play&#0045;2"
 								target="_blank"
-							><img
+							>
+								<img
 									src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=416946&theme=light&t=1752143504408"
 									alt="Vue&#0032;Play - Create&#0032;Vue&#0032;Applications&#0032;using&#0032;a&#0032;Visual&#0032;Designer | Product Hunt"
 									style="width: 250px; height: 54px;;margin:auto"
 									width="250"
 									height="54"
-								/></a> </Box>
+								/>
+							</a>
+						</Box>
 					</Box>
 					<Box
 						gap="{`default`:{`xs`:{`light`:`50px`}}}"
@@ -376,16 +396,21 @@
 							alignItems="{`default`:{`xs`:{`light`:`center`}}}"
 							overflow="{`default`:{`xs`:{`light`:`visible`}}}"
 							padding="{`default`:{`xs`:{`light`:`0px`}}}"
-						><a
+						>
+							<a
 								href="https://madewithvuejs.com/p/vue-play/shield-link"
 								target="_blank"
 								class="inline-block mx-auto"
-								data-allow-mismatch=""
-							><img src="https://madewithvuejs.com/storage/repo-shields/4476-shield.svg" /></a> </Box>
+								data-allow-mismatch
+							>
+								<img src="https://madewithvuejs.com/storage/repo-shields/4476-shield.svg" />
+							</a>
+						</Box>
 					</Box>
 				</Box>
 			</Box>
-		</template> <template #footer="">
+		</template>
+		<template #footer>
 			<Footer></Footer>
 		</template>
 	</Home>
